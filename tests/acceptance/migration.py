8c9ece65aeceada48a7829acd43cdf53e01d7bd5
ovoshcha <ovoshcha@redhat.com>
2019-12-10 19:47:15 +0200

    Acceptance test: provides to use RDMA transport for migration
    Provides param 'address' in _get_free_port()
    because by default it takes free port on the localhost
    Adds new test with migration through RDMA
    
    V2 - remove rxe_cfg

--- tests/acceptance/migration.py
+++ tests/acceptance/migration.py
@@ -10,16 +10,23 @@
 # later.  See the COPYING file in the top-level directory.
 
 
+import re
 import tempfile
-from avocado_qemu import Test
+import netifaces
 from avocado import skipUnless
+from avocado_qemu import Test
 
 from avocado.utils import network
 from avocado.utils import wait
 from avocado.utils.path import find_command
+from avocado.utils import service
+from avocado.utils import process
 
 
 class Migration(Test):
+    """
+    :avocado: tags=migration
+    """
 
     timeout = 10
 
@@ -38,20 +45,35 @@ class Migration(Test):
         self.assertEqual(source_vm.command('query-status')['status'], 'postmigrate')
 
     def do_migrate(self, dest_uri, src_uri=None):
-        source_vm = self.get_vm()
         dest_vm = self.get_vm('-incoming', dest_uri)
+        dest_vm.add_args('-nodefaults')
         dest_vm.launch()
         if src_uri is None:
             src_uri = dest_uri
+        source_vm = self.get_vm()
+        source_vm.add_args('-nodefaults')
         source_vm.launch()
         source_vm.qmp('migrate', uri=src_uri)
         self.assert_migration(source_vm, dest_vm)
 
-    def _get_free_port(self):
+    def _get_free_port(self, address='localhost'):
         port = network.find_free_port()
         if port is None:
             self.cancel('Failed to find a free port')
         return port
+
+    def _if_rdma_enable(self):
+        rdma_stat = service.ServiceManager()
+        rdma = rdma_stat.status('rdma')
+        return rdma
+
+    def _get_ip_rdma(self):
+        get_ip_rdma = process.run('rdma link show').stdout.decode()
+        for line in get_ip_rdma.split('\n'):
+            if re.search(r"ACTIVE", line):
+                interface = line.split(" ")[-2]
+                ip = netifaces.ifaddresses(interface)[netifaces.AF_INET][0]['addr']
+                return ip
 
 
     def test_migration_with_tcp_localhost(self):
@@ -72,3 +94,11 @@ class Migration(Test):
         dest_uri = 'exec:nc -l localhost %u' % free_port
         src_uri = 'exec:nc localhost %u' % free_port
         self.do_migrate(dest_uri, src_uri)
+
+    @skipUnless(_if_rdma_enable(None), "Unit rdma.service could not be found")
+    @skipUnless(_get_ip_rdma(None), 'RoCE(RDMA) service or interface not configured')
+    def test_migration_with_rdma_localhost(self):
+        ip = self._get_ip_rdma()
+        free_port = self._get_free_port(address=ip)
+        dest_uri = 'rdma:%s:%u' % (ip, free_port)
+        self.do_migrate(dest_uri)
