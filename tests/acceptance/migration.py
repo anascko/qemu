d376454ffa6a07cdaf5124ad5eccddcf74192866
ovoshcha <ovoshcha@redhat.com>
2019-12-26 15:58:11 +0200

    Acceptance test: provides to use EXEC transport for migration
    Along with VM migration via TCP, we can use migration through EXEC

--- tests/acceptance/migration.py
+++ tests/acceptance/migration.py
@@ -11,10 +11,10 @@
 
 
 from avocado_qemu import Test
-
+from avocado import skipUnless
 from avocado.utils import network
 from avocado.utils import wait
-
+from avocado.utils.path import find_command, CmdNotFoundError
 
 class Migration(Test):
 
@@ -24,12 +24,21 @@ class Migration(Test):
     def migration_finished(vm):
         return vm.command('query-migrate')['status'] in ('completed', 'failed')
 
+    def assert_migration(self, source_vm, dest_vm):
+        wait.wait_for(self.migration_finished,
+                      timeout=self.timeout,
+                      step=0.1,
+                      args=(source_vm,))
+        self.assertEqual(source_vm.command('query-migrate')['status'], 'completed')
+        self.assertEqual(dest_vm.command('query-migrate')['status'], 'completed')
+        self.assertEqual(dest_vm.command('query-status')['status'], 'running')
+        self.assertEqual(source_vm.command('query-status')['status'], 'postmigrate')
+
     def _get_free_port(self):
         port = network.find_free_port()
         if port is None:
             self.cancel('Failed to find a free port')
         return port
-
 
     def test_migration_with_tcp_localhost(self):
         source_vm = self.get_vm()
@@ -38,13 +47,16 @@ class Migration(Test):
         dest_vm.launch()
         source_vm.launch()
         source_vm.qmp('migrate', uri=dest_uri)
-        wait.wait_for(
-            self.migration_finished,
-            timeout=self.timeout,
-            step=0.1,
-            args=(source_vm,)
-        )
-        self.assertEqual(dest_vm.command('query-migrate')['status'], 'completed')
-        self.assertEqual(source_vm.command('query-migrate')['status'], 'completed')
-        self.assertEqual(dest_vm.command('query-status')['status'], 'running')
-        self.assertEqual(source_vm.command('query-status')['status'], 'postmigrate')
+        self.assert_migration(source_vm, dest_vm)
+
+    @skipUnless(find_command('nc', default=False), "nc command not found on the system")
+    def test_migration_with_exec(self):
+        free_port = self._get_free_port()
+        source_vm = self.get_vm()
+        dest_uri = 'exec:nc -l localhost %u' % free_port
+        src_uri = 'exec:nc localhost %u' % free_port
+        source_vm.launch()
+        dest_vm = self.get_vm('-incoming', dest_uri)
+        dest_vm.launch()
+        source_vm.qmp('migrate', uri=src_uri)
+        self.assert_migration(source_vm, dest_vm)
